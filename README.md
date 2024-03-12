e30b80757b50717de9509a66e81a5573bbabd2af
Doaa Mohamed <mohammaddoaa64@gmail.com>
2024-03-12 21:03:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blog-React-app
+# Blog-React-app
---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from "react";
+import LatestBlogs from "../miniComponents/LatestBlogs";
+import HeroSection from "../miniComponents/HeroSection";
+import { Context } from "../../main";
+
+const Home = () => {
+  const { mode, blogs } = useContext(Context);
+  const filteredBlogs = blogs.slice(0, 6);
+  return (
+    <>
+      <article className={mode === "dark" ? "dark-bg" : "light-bg"}>
+        <HeroSection />
+        <LatestBlogs heading={"Latest Blogs"} blogs={filteredBlogs} />
+      </article>
+    </>
+  );
+};
+
+export default Home;
